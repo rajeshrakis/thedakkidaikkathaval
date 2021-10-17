8084af7e3c664044316e6012773e21e7da2715d0
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2021-10-17 17:01:43 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m ThedakkidaikkathavalBot
+web: python3 -m ThedakkidaikkathavalBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE 𝖙𝖍𝖊𝖉𝖆 𝕶𝖎𝖉𝖆𝖎𝖐𝖐𝖆𝖙𝖍𝖆𝖛𝖆𝖑
+:: Enables virtual env mode and then starts thedakkidaikkathaval
+env\scripts\activate.bat && py -m ThedakkidaikkathavalBot
